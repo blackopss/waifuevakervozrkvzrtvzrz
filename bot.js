85a4cdfe98312aa50200e7c38a94dd699ec7f1ac
blackopss <36933564+blackopss@users.noreply.github.com>
2018-07-11 21:54:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,7 +95,7 @@ client.on('message', async msg => { // eslint-disable-line
 			        .setDescription(`**could you please choose song number** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-					.setFooter("! Roses.")
+					.setFooter("Wafiu")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
